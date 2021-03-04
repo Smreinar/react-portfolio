dee1c6ef27156e0b4277493c57fe96c0409930c1
smreinar <stevenreinart123@gmail.com>
2021-03-04 08:48:42 -0700

    React app Skeleton

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// Import Nvabar
+// Import Aboutme
+// Import Portfolio
+// Import Socail medai
+// Import react/component
+import './App.css';
+
+class App extends Component{
+  
+  
+  //SubClass render() return
+  render() {
+    return(
+      {/* Navbar */}
+      {/* Aboutme */}
+      {/* Portfolio */}
+      {/* Socail Media */}
+      
+    )
+  }
+}
+
+export default App;
