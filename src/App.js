9669ddd151c47d3904393205941bb7fc585c9d53
smreinar <stevenreinart123@gmail.com>
2021-03-08 19:38:53 -0700

    Socail Media added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // Import react/component
 import projects from "./portfolio.json"; // Import portfolio libary
-import Portfolio from "./components/Portfolio"; // Import Portfolio
-// Import Socail medai
-// Import react/component
+import platforms from "./platforms.json";
+import Navbar from './components/Navbar'; // Import Nvabarcomponent
+import Aboutme from './components/Aboutme'; // Import Aboutme component
+import Portfolio from "./components/Portfolio"; // Import Portfolio component
+import Socailmedia from './components/Socialmedia'; // Import Socail media component
 import './App.css';
-import Navbar from './components/Navbar'; // Import Nvabar
-import Aboutme from './components/Aboutme'; // Import Aboutme
-
 class App extends Component{
   
   state = {
-    projects
+    projects,
+    platforms
   };
  
   
@@ -19,23 +19,39 @@ class App extends Component{
     return(
       <div className="container">
         <Navbar></Navbar>
-      {/* Aboutme */}
-      <Aboutme></Aboutme>
-      {/* Portfolio */}
-        <div className="card-deck">
-          {this.state.projects.map(projects => (
-          <Portfolio
+        {/* Aboutme */}
+        <Aboutme></Aboutme>
+        {/* Portfolio */}
+        <div className="row">
+          <div className="card-deck">
+            {this.state.projects.map(projects => (
+              <Portfolio
           
-          key={projects.id}
-          id={projects.id}
-          name={projects.name}
-          image={projects.image}
-          link={projects.link}
-          ></Portfolio> 
-          ))}
+              key={projects.id}
+              id={projects.id}
+              name={projects.name}
+              image={projects.image}
+              link={projects.link}
+              ></Portfolio> 
+            ))}
+          </div>
+        </div>
+            {/* Socail Media */}
+        <div className="row justify-content-md-center">
+          <div className="card-deck">
+            {this.state.platforms.map(platforms => (
+            <Socailmedia
+          
+            key={platforms.id}
+            id={platforms.id}
+            name={platforms.name}
+            image={platforms.image}
+            link={platforms.link}
+            ></Socailmedia> 
+            ))}
+          </div>
         </div>
       </div>
-     
     )
   }
 }
--- src/components/Socialmedia/index.js
+++ src/components/Socialmedia/index.js
@@ -1,2 +1,17 @@
 import React from "react";
 import "./socialmstylesheet.css";
+
+
+const Socailmedia = props => (
+    <div className="col" >
+    <div className="card mx-auto" style={{height: 50, width: 50}}>
+        <a href={props.link}>
+        <img className="card-img-top" alt={props.name} src={props.image}/>
+        <div className="card-body">
+        </div>
+        </a>
+    </div>
+    </div>
+)
+
+export default Socailmedia;
